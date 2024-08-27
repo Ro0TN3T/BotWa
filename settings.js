b7605d81255fa2e77f5b08fc35a5fafc6838b684
NcPulsa <46824241+Ro0TN3T@users.noreply.github.com>
2024-08-28 03:57:05 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.devNumber = ['6282162627474'] //Pake nomor mu
+global.name = 'Ro0T_N3T' //Isi nama mu
+global.namebot = 'Ro0T_N3T' //Isi nama Bot mu
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.blueBright(`Update File Terbaru ${__filename}`))
+delete require.cache[file]
+require(file)
+})
